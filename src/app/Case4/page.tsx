bb68fb2cee53a8fff7a0e5335b49920e3a99c050
baikong <JayLyu@users.noreply.github.com>
2024-04-13 15:05:06 +0800

    增加Gallery effect

---
+++ src/app/Case4/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./page.module.scss";
+
+
+const Case4 = () => {
+  const imgs = [
+    "https://plus.unsplash.com/premium_photo-1694161845354-f1e829687c03?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+    "https://plus.unsplash.com/premium_photo-1680831748238-1fe52ad0e3af?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
+    "https://plus.unsplash.com/premium_photo-1692897456940-982f81bfa0c3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1712833027661-b2a82a5d7a96?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1712251351536-7bda2a48e929?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+  return (
+    <div className={styles.container}>
+      <h2>Gallery Effect</h2>
+
+      <div className={styles.gallery}>
+        {imgs.map((item, index) => {
+          return (
+            <div key={index} className={styles.galleryPanel}>
+              <picture>
+                <img src={item} alt="image" />
+              </picture>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Case4;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <Link href={'/Case1'}><div className={styles.card}>Case 1</div></Link>
         <Link href={'/Case2'}><div className={styles.card}>Case 2</div></Link>
         <Link href={'/Case3'}><div className={styles.card}>Case 3</div></Link>
+        <Link href={'/Case4'}><div className={styles.card}>Gallery Effect</div></Link>
       </section>
     </main>
   )
