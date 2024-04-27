a73c01667319d289f7a38e79b24b80c84263bb62
baikong <JayLyu@users.noreply.github.com>
2024-04-27 22:51:54 +0800

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -12,5 +12,6 @@ const withMDX = createMDX({
   // Add markdown plugins here, as desired
 })
  
+module.exports = nextConfig
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig)
